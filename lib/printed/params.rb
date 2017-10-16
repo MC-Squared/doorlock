9c83e654ca2c74896a625b9bce3279e4b89b8d6e
J Miles <j.milesy.nz@gmail.com>
2017-10-16 21:18:03 +1300

    Adjust param sizes

--- lib/printed/params.rb
+++ lib/printed/params.rb
@@ -1,11 +1,11 @@
-$base_x = 50
+$base_x = 55
 $base_y = 35
 $base_z = 2
 
-$anchor_t = 5
+$anchor_t = 6
 $anchor_w = $anchor_t*1.5
 $anchor_l = $base_y * 0.9
 
 $hook_l = 90
-$latch_l = 20
+$latch_l = 30
 $latch_t = $anchor_t - 1.5
